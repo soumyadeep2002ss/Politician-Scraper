dd71e396e93e3668afe20841be90bfbb62ce08e1
Soumyadeep Pal <64325089+soumyadeep2002ss@users.noreply.github.com>
2024-01-26 09:42:46 +0530

    Modify the prompt

--- llm/googleai.py
+++ llm/googleai.py
@@ -4,6 +4,7 @@ import json
 
 import google.generativeai as genai
 
+
 def extract_text_from_pdf(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         text = ''
@@ -11,31 +12,42 @@ def extract_text_from_pdf(pdf_path):
             text += page.extract_text()
     return text
 
+
 def query_openai(text):
-    GOOGLE_API_KEY = 'GOOGLE_API_KEY' # add your GOOGLE_API_KEY
+    GOOGLE_API_KEY = 'AIzaSyDd26SvuTQqx5kIW50llUWnWCMtP4bZpWg'  # add your GOOGLE_API_KEY
     genai.configure(api_key=GOOGLE_API_KEY)
 
-    prompt = f"Extract the name, address, and position of the politician from the following text:\n\n{text}"
+    prompt = f"""Given the following text, extract and format the name, address, and position of the politician:
+
+Text:
+{text}
+
+Instructions:
+- The name should be formatted as "Name: [politician's name]"
+- The address should be formatted as "Address: [politician's address]"
+- The position should be formatted as "Position: [politician's position]"
+"""
     model = genai.GenerativeModel('gemini-pro')
     messages = [
-    {'role':'user',
-     'parts': [prompt]}
+        {'role': 'user',
+         'parts': [prompt]}
     ]
     response = model.generate_content(messages)
 
     # Sending request to OpenAI
     # response = openai.Completion.create(
-    #     engine="gpt-3.5-turbo-instruct", 
+    #     engine="gpt-3.5-turbo-instruct",
     #     prompt=prompt,
-    #     max_tokens=150  
+    #     max_tokens=150
     # )
-    
+
     # return response.choices[0].text.strip()
     print(response.text)
     return response
 
+
 # add the path to your PDF
-pdf_path = 'Juan_Lucas_Restrepo_final.pdf'
+pdf_path = 'Christopher G Cavolli_test.pdf'
 
 text = extract_text_from_pdf(pdf_path)
 
