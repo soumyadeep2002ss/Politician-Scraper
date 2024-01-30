b0996859d697f724abf63cd0b8afd90dafcba0b4
ManamiMondal <84272920+ManamiMondal@users.noreply.github.com>
2024-01-31 01:50:14 +0530

    merged the safety_settings

--- llm/googleai.py
+++ llm/googleai.py
@@ -34,7 +34,30 @@ def  query_using_gemini(text, field, politician, api_key):
     messages = [
         {'role':'user', 'parts': [prompt]}
     ]
-    response = model.generate_content(messages)
+    safety_settings = [
+        {
+            "category": "HARM_CATEGORY_DANGEROUS",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE",
+        },
+    ]
+    response = model.generate_content(
+        messages, safety_settings=safety_settings)
     print(response.text)
     return response.text
 
