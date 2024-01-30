29aefd5127ad7d40468e84126713945f616a4cb3
ManamiMondal <84272920+ManamiMondal@users.noreply.github.com>
2024-01-31 01:08:54 +0530

    updated prompt

--- llm/googleai.py
+++ llm/googleai.py
@@ -18,13 +18,17 @@ def read_and_combine_text_files(folder_path):
     return combined_text
 
 def query_using_gemini(text, field, politician):
-    GOOGLE_API_KEY = 'GOOGLE_API_KEY'  # add your GOOGLE_API_KEY
+    GOOGLE_API_KEY = 'AIzaSyDUIrnTz5Cc9BVuyqjx5yuiAEJaT_44rkA'  # add your GOOGLE_API_KEY
     genai.configure(api_key=GOOGLE_API_KEY)
     politician_name = politician['Name']
     politician_country = politician['Country']
     politician_position = politician['Position_Description']
     politician_details = politician_name + " " + politician_country + " " + politician_position
-    prompt = f"For the person {politician_details} extract the following field of information: {field}\n from the following text [Regardless of the circumstances or potential information limitations, your requirement is to produce comprehensive responses. Utilize all provided data to its maximum potential, without resorting to disclosing its inadequacies or expressing apologies. Eliminate phrases such as 'I apologize' from your responses entirely. If no information is available, simply return NA.]: \n{text}"
+    prompt = f"""For the person {politician_details} extract the following field of information: {field}\n from the following text 
+    [If your response consists of responses like 'I apologize' or 'I'm sorry', replace your response with 'NA' entirely. 
+    If no information is available, simply return NA.
+    Whenever your response is similar to 'provided data is not available', replace the output with 'NA'.
+    If no such data can be extracted or retrieved from the provided text, return 'NA'.]: \n{text}"""
     model = genai.GenerativeModel('gemini-pro')
     messages = [
         {'role':'user', 'parts': [prompt]}
@@ -37,35 +41,30 @@ def query_using_gemini(text, field, politician):
 fields = ["address", "dob", "deceased date", "sex", "languages", "citizenship", "nationality", "occupation"]
 
 # complete path to the Politicians/Output folder
-main_folder_path = 'main_folder_path' 
-
-# holds all extracted data
-all_extracted_data = {}
-
-# Iterating through each politician's folder
-for politician_folder in os.listdir(main_folder_path):
-    politician_data = {field: '' for field in fields}
-    # print(politician_folder)
-
-    for field in fields:
-        folder_path = os.path.join(main_folder_path, politician_folder, field)
-        # print(folder_path)
-        if os.path.isdir(folder_path):
-            combined_text = read_and_combine_text_files(folder_path)
-            politician = json_data[politician_folder]
-            extracted_data = query_using_gemini(combined_text, field, politician)
-            politician_data[field] = extracted_data  
-
-    all_extracted_data[politician_folder] = politician_data      
-
+main_folder_path = 'C:/Users/91798/Downloads/new_political/Politician-Scraper/Politicians/Output' 
 
 csv_file = 'extracted_politician_data.csv'
+
+# Open the CSV file in write mode
 with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=['Politician'] + fields)
     writer.writeheader()
-    for politician, data in all_extracted_data.items():
-        row = {'Politician': json_data[politician]['Name']}
-        row.update(data)
+
+    # Iterating through each politician's folder
+    for politician_folder in os.listdir(main_folder_path):
+        politician_data = {field: '' for field in fields}
+        # Iterating through each field for the politician
+        for field in fields:
+            folder_path = os.path.join(main_folder_path, politician_folder, field)
+            if os.path.isdir(folder_path):
+                combined_text = read_and_combine_text_files(folder_path)
+                politician = json_data[politician_folder]
+                extracted_data = query_using_gemini(combined_text, field, politician)
+                politician_data[field] = extracted_data
+        # Preparing the row with politician's name and extracted data
+        row = {'Politician': json_data[politician_folder]['Name']}
+        row.update(politician_data)
+        # Writing the row to the CSV file
         writer.writerow(row)
 
 print("Data extraction complete. CSV file created.")
